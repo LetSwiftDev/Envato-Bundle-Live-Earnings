a7a10c2f76d0efc2d13220cb1f8e80165bab0849
cfconsultancy <cfconsultancy@gmail.com>
2014-08-16 23:18:09 +0200

    Update bundle.php
    added FlipClock

--- bundle.php
+++ bundle.php
@@ -27,8 +27,31 @@ Dont change anything here ! Edit only math.php
 <meta charset="UTF-8">
 <title>Envato - Bundle Sales - Live Earning</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="flipclock.min.js"></script>
+<link href="flipclock.css" rel="stylesheet" type="text/css" />
 <script type="text/javascript">// <![CDATA[
+var clock;
 $(document).ready(function() {
+
+  /************* FLIPCLOCK *************/
+
+  // Grab the current date
+  var currentDate = new Date();
+
+  // Set some date in the future. In this case, it's always Jan 1
+  var futureDate  = new Date(currentDate.getFullYear(), 7, 20,12);
+
+
+  // Calculate the difference in seconds between the future and current date
+  var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+  // Instantiate a coutdown FlipClock
+  clock = $('.clock').FlipClock(diff, {
+  clockFace: 'DailyCounter',
+  countdown: true,
+  showSeconds: true
+});
+
 $.ajaxSetup({ cache: false }); // This part addresses an IE bug.  without it, IE will only load the first number and will never refresh
   function instantGratification( fn, delay ) {
       fn();
@@ -52,8 +75,17 @@ body {
 	margin-right: 0px;
 	margin-bottom: 0px;
 }
+#liveclock {
+    width:700px;
+    text-align:center;margin:0 auto;
+}
+#results {
+	margin-bottom:40px;
+}
 </style>
 </head>
-<body><div id="results">Loading ...</div>
+<body>
+<div id="results">Loading ...</div>
+<div id="liveclock"><div class="clock"></div></div>
 </body>
 </html>
